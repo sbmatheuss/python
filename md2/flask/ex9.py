5c4569dc00787a3e69f75a6fe9773cfef8451236
sbmatheuss <sbmatheus20@gmail.com>
2026-08-01 22:55:29 -0300

    Add módulo 2 ex9 (Flask + Jinja: render_template com perfil)

---
+++ md2/flask/ex9.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route("/perfil")
+def perfil():
+    nome = "Matheus"
+    idade = "23"
+    return render_template("perfil.html", nome=nome, idade=idade)
+
+if __name__ == "__main__":
+    app.run(debug=True)
