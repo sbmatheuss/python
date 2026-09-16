549f3d61c3778adae4e3c841d1b108a60a603463
Matheus Braga <sbmatheus20@gmail.com>
2026-09-16 11:50:53 -0300

    Acessando elementos de uma litas Modificando valores de lista Usando valores individuais de uma lista

--- lbook/float.py
+++ lbook/float.py
@@ -3,3 +3,5 @@ print(10-2) # subtração
 print(4*2) # multiplicação
 print(16/2) # divisão
 
+favorite_number = 9
+print(f"Número favorito: {favorite_number}")
---
+++ lbook/listas/listas.py
@@ -0,0 +1,10 @@
+names = ['bruno', 'ivo', 'noah']
+print(f"Olá, {names[0]}") # retorna o primeiro nome da lista
+print(f"Bom dia {names[1]}") # retorna o segundo nome da lista
+print(f"Bom tarde {names[2]}") # retorna o terceiro nome da lista e assim sucessivamente
+
+motos = ['Yamaha', 'Honda', 'BMW']
+print(f"Gostaria de ter uma moto {motos[0]} do modelo FZ25")
+print(f"Dizem que moto {motos[1]} da muito problema")
+print(f"A {motos[2]} só produzem motos de alto padrão")
+
