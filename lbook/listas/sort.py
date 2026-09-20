cd948ee9d9bc0e83d8ca23f1d35200c682a216a4
sbmatheuss <sbmatheus20@gmail.com>
2026-09-20 20:04:52 -0300

    Criando nova lista com os mesmos elementos com o método sort()

---
+++ lbook/listas/sort.py
@@ -0,0 +1,10 @@
+
+
+numeros = [7, 3, 8, 1]
+crescente = sorted(numeros) # criou uma nova lista com os mesmos elementos da original e retornou em ordem crescente
+decrescente = sorted(numeros, reverse=True) # criou uma nova lista com os mesmos elementos da original e retornou em ordem decrescent
+print(crescente)
+print(decrescente)
+
+# sorted() cria uma nova lista com os mesmo elementos da original com possibilidade de alteração
+# mantendo a lista original intacta
