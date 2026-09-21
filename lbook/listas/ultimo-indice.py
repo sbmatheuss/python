3e9569fd8f9dc38aa0c36b3c47d8a31e8c73e9c7
Matheus Braga <sbmatheus20@gmail.com>
2026-09-21 17:47:26 -0300

    Teste de erro retornando -1 em uma lista vazia

--- lbook/listas/ultimo-indice.py
+++ lbook/listas/ultimo-indice.py
@@ -3,3 +3,6 @@
 
 frutas = ['pera', 'maça', 'uva', 'goiaba']
 print(frutas[-1]) # consulta o último elemento da lista
+
+objetos = [] # teste de erro retornando -1 em lista vazia
+print(objetos[-1]) 
